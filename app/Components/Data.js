136950a729451547a1f86433b13a8a7f6d9f4045
Healer2003 <wwww.wilberforce221@gmail.com>
2024-06-25 17:41:56 +0000

    update

--- app/Components/Data.js
+++ app/Components/Data.js
@@ -3,21 +3,21 @@ export const items = [
     {
         id:"1",
         name:"Sent",
-        icon:<AntDesign name="arrowup" size={20} color={"black"} />
+        icon:<AntDesign name="arrowup" size={25} color={"black"} />
     },
     {
         id:"2",
         name:"Receive",
-        icon:<AntDesign name="arrowdown" size={20} color={"black"}/>
+        icon:<AntDesign name="arrowdown" size={25} color={"black"}/>
     },
     {
         id:"3",
         name:"Loan",
-        icon:<FontAwesome6 name="dollar-sign" size={20} color={"black"}/>
+        icon:<FontAwesome6 name="dollar-sign" size={25} color={"black"}/>
     },
     {
         id:"4",
         name:"Topup",
-        icon:<Ionicons name="cloud-upload-outline" size={20} color={"black"}/>
+        icon:<Ionicons name="cloud-upload-outline" size={25} color={"black"}/>
     }
 ]
