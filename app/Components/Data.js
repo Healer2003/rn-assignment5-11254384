61d73406021165fc5a45abbae771c8e8f61fe50d
Healer2003 <wwww.wilberforce221@gmail.com>
2024-06-25 17:07:59 +0000

    created file for dataitems

---
+++ app/Components/Data.js
@@ -0,0 +1,23 @@
+import {Ionicons,AntDesign} from '@expo/vector-icons'
+export const items = [
+    {
+        id:"1",
+        name:"Sent",
+        icon:<AntDesign name="arrowup" size={20} color={"black"} />
+    },
+    {
+        id:"2",
+        name:"Receive",
+        icon:<AntDesign name="arrowdown" size={20} color={"black"}/>
+    },
+    {
+        id:"3",
+        name:"Loan",
+        icon:<AntDesign name="dollar" size={20} color={"black"}/>
+    },
+    {
+        id:"4",
+        name:"Topup",
+        icon:<Ionicons name="cloud-upload-outline" size={20} color={"black"}/>
+    }
+]
