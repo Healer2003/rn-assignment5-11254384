3c89a066d4dc46f337da1f70d1066296cffea674
Healer2003 <wwww.wilberforce221@gmail.com>
2024-06-26 09:31:54 +0000

    added transactions card component

---
+++ app/Components/Transaction.js
@@ -0,0 +1,10 @@
+
+export default function Transaction(){
+    return(
+        <View>
+            <View></View>
+            <View></View>
+            <View></View>
+        </View>
+    )
+}
--- app/Screens/Home.js
+++ app/Screens/Home.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet,View } from "react-native"
+import { Image, StyleSheet,View,Text } from "react-native"
 import UserCard from "../Components/UserCard"
 import { items } from "../Components/Data"
 import IconCard from "../Components/IconCard"
@@ -15,11 +15,15 @@ export default function Home(){
                 {
                     items.map((item,index)=>{
                         return(
-                            <IconCard name={item.name} icon={item.icon} key={index.id}/>
+                            <IconCard name={item.name} icon={item.icon} key={item.id}/>
                         )
-                       
                     })
                 }
+            </View>
+
+            <View style={{flexDirection:"row",justifyContent:"space-between",alignItems:"center",marginVertical:20}}>
+                <Text style={{fontSize:20}}>Transaction</Text>
+                <Text style={{color:"blue"}}>Sell All</Text>
             </View>
         </View>
     )
